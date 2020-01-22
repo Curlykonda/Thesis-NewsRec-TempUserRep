9f82273f4ef01eed25b80dc759e3196b2392af0a
Curlykonda <bartsch.henning@gmail.com>
2020-01-22 09:12:16 +0100

    initial

---
+++ preprocessing/prep-amazon-books.py
@@ -0,0 +1,29 @@
+import os
+import json
+import gzip
+import pickle
+import numpy as np
+
+import pandas as pd
+from urllib.request import urlopen
+import matplotlib.pyplot as plt
+from datetime import datetime
+from collections import defaultdict
+from tqdm import tqdm
+
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+#nltk.download()
+nltk.download('stopwords')
+nltk.download('punkt')
+
+
+
+
+
+
+if __name__ == "__main__":
+    raise NotImplementedError("Not implemented yet")
