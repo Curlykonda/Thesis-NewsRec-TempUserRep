b9d41eb315a19d743c6cb23417e881ce43fd217c
Curlykonda <bartsch.henning@gmail.com>
2020-03-18 17:53:04 +0100

    add try_var_losses demo

--- train_scripts/test_losses.py
+++ train_scripts/test_losses.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import random
 import torch
 import torch.nn as nn
 import sys
@@ -19,12 +19,20 @@ def test_losses():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
     batch_size = 100
+    n_classes = 5
 
     loss_fn = nn.BCEWithLogitsLoss()
     input = torch.randn(batch_size, requires_grad=True).to(device)
     target = torch.empty(batch_size).random_(2).to(device)
     loss = loss_fn(input, target)
     loss.backward()
+
+
+    logits = torch.randn((batch_size, n_classes), requires_grad=True).to(device)
+    lbls = [0] * (n_classes-1) + [1]
+    targets = torch.tensor([random.sample(lbls, len(lbls)) for _ in range(batch_size)]).float().to(device)
+
+    try_var_loss_funcs(logits, targets, 1)
 
 if __name__ == "__main__":
     test_losses()
