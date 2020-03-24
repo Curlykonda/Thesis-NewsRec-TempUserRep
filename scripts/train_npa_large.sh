4c01cbcb7b6b817dac058f6856f960760c9ff6e7
Curlykonda <bartsch.henning@gmail.com>
2020-03-24 17:01:20 +0100

    more time for large

--- scripts/train_npa_large.sh
+++ scripts/train_npa_large.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_npa_large
-#SBATCH -n 4
-#SBATCH -t 01:00:00
+#SBATCH -n 8
+#SBATCH -t 02:00:00
 #SBATCH -p gpu_shared
 #SBATCH --gres=gpu:1
 #SBATCH --mem=60000M
