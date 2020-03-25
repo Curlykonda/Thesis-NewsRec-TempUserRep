dc6d3e0865f6e4f06291170a190ac63be6565ebb
Curlykonda <bartsch.henning@gmail.com>
2020-03-25 10:51:20 +0100

    #1 Experiment 'weight_decay'

--- scripts/train_npa_large.sh
+++ scripts/train_npa_large.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_npa_large
 #SBATCH -n 8
-#SBATCH -t 02:00:00
+#SBATCH -t 12:00:00
 #SBATCH -p gpu_shared
 #SBATCH --gres=gpu:1
 #SBATCH --mem=60000M
@@ -20,7 +20,8 @@ cd $workdir/train_scripts
 python --version
 
 python -u train_npa.py --data_path="../datasets/dpg/i100k_u50k_s30/" --word_emb_path="../embeddings/cc.nl.300.bin" --test_w_one=1 --exp_name="large"
-python -u train_npa.py --data_path="../datasets/dpg/i100k_u50k_s30/" --word_emb_path="../embeddings/cc.nl.300.bin" --test_w_one=1 --random_seed=13 --exp_name="large"
-python -u train_npa.py --data_path="../datasets/dpg/i100k_u50k_s30/" --word_emb_path="../embeddings/cc.nl.300.bin" --test_w_one=1 --random_seed=102 --exp_name="large"
+python -u train_npa.py --data_path="../datasets/dpg/i100k_u50k_s30/" --word_emb_path="../embeddings/cc.nl.300.bin" --weight_decay=0.0001 --test_w_one=1 --exp_name="large-wd1e4"
+#python -u train_npa.py --data_path="../datasets/dpg/i100k_u50k_s30/" --word_emb_path="../embeddings/cc.nl.300.bin" --test_w_one=1 --random_seed=13 --exp_name="large"
+#python -u train_npa.py --data_path="../datasets/dpg/i100k_u50k_s30/" --word_emb_path="../embeddings/cc.nl.300.bin" --test_w_one=1 --random_seed=102 --exp_name="large"
 
 
