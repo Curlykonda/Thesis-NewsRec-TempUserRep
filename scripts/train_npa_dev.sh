ac892028a200a176fba8e676c00bee2f77ae0991
Curlykonda <bartsch.henning@gmail.com>
2020-03-31 14:37:15 +0200

    remove "test_w_one"

--- scripts/train_npa_dev.sh
+++ scripts/train_npa_dev.sh
@@ -19,17 +19,17 @@ cd $workdir/train_scripts
 
 python --version
 
-python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --test_w_one=0 --exp_name="dev"
+python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --exp_name="dev"
 
-python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --test_w_one=0 --exp_name="dev-softm"
+python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --exp_name="dev-softm"
                   --eval_method="custom" --train_act_func="softmax" --test_act_func="softmax"
 
-python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --test_w_one=0 --exp_name="dev-sig"
+python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --exp_name="dev-sig"
                   --eval_method="custom" --train_act_func="sigmoid" --test_act_func="sigmoid"
 
-python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --weight_decay=0.0001 --test_w_one=0 --exp_name="dev-wd" --eval_method="custom"
+python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --weight_decay=0.0001 --exp_name="dev-wd" --eval_method="custom"
 
-python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --weight_decay=0.001 --test_w_one=0 --exp_name="dev-wd" --eval_method="custom"
+python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --weight_decay=0.001 --exp_name="dev-wd" --eval_method="custom"
 #python -u train_npa.py --data_path="../datasets/dpg/dev/" --word_emb_path="../embeddings/cc.nl.300.bin" --weight_decay=0.0001 --test_w_one=1 --random_seed=13 --exp_name="dev-wd"
 #python -u train_npa.py --data_path="../datasets/dpg/dev/" --word_emb_path="../embeddings/cc.nl.300.bin" --weight_decay=0.0001 --test_w_one=1 --random_seed=102 --exp_name="dev-wd"
 
