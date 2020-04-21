acf61d649a7e4fec2d0c3c59a8a32232b4c6f754
HB <bartsch.henning@gmail.com>
2020-04-21 11:36:54 +0200

    Adjust 'lambda_l2' for preliminary experiments to investigate low AUC in current 'master' branch [21.4.]

--- train_scripts/train_npa.py
+++ train_scripts/train_npa.py
@@ -135,7 +135,7 @@ def test_eval_like_npa_wu(model, test_generator, act_func="sigmoid", one_candida
 
             # compute loss
             test_loss = nn.BCELoss()(y_probs, lbls)
-            #
+            # Original NPA evaluation method:
             # aggregate predictions per user because each user can have various test samples
             for idx, u_id in enumerate(user_ids):
                 u_id = u_id.item()
@@ -159,8 +159,8 @@ def test_eval_like_npa_wu(model, test_generator, act_func="sigmoid", one_candida
 
             # precision, recall, thresholds = precision_recall_curve(y_true, y_scores)
             if device.type == 'cpu':
-                #break
-                pass
+                break
+                #pass
     loss, acc, raw_scores = (zip(*metrics_epoch))
 
     metrics = defaultdict(list)
@@ -225,8 +225,7 @@ def train_npa_actfunc(npa_model, criterion, optim, train_generator, act_func="so
             else:
                 loss_l2 = loss_l2 + param.norm(2)
 
-        loss_l2 = loss_l2 * config.lambda_l2
-        loss_total = loss_bce + loss_l2
+        loss_total = loss_bce + loss_l2 * config.lambda_l2
 
         # optimiser backward
         optim.zero_grad()
@@ -451,7 +450,7 @@ if __name__ == "__main__":
 
     # input data
     parser.add_argument('--data_type', type=str, default='DPG', help='options for data format: DPG, NPA or Adressa ')
-    parser.add_argument('--data_path', type=str, default='../datasets/dpg/dev_time_split/', help='path to data directory') # dev : i10k_u5k_s30/
+    parser.add_argument('--data_path', type=str, default='../datasets/dpg/dev_time_split_interactions/', help='path to data directory') # dev : i10k_u5k_s30/
     parser.add_argument('--word_emb_path', type=str, default='../embeddings/glove_eng.840B.300d.txt',
                         help='path to directory with word embeddings')
 
@@ -464,7 +463,7 @@ if __name__ == "__main__":
                         help='Negative sample ratio N: for each positive impression generate N negative samples')
     parser.add_argument('--candidate_generation', type=str, default='neg_sampling',
                         help='Method to generate candidate articles: [neg_sampling, neg_sampling_time]')
-    parser.add_argument('--train_method', type=str, default='pos_cut_off',
+    parser.add_argument('--train_method', type=str, default='wu',
                         help='Method for network training & format of training samples: [wu, pos_cut_off, masked_interests]')
 
     #model params
@@ -474,7 +473,7 @@ if __name__ == "__main__":
     #training
     parser.add_argument('--batch_size', type=int, default=100, help='batch size for training')
     parser.add_argument('--n_epochs', type=int, default=10, help='Epoch number for training')
-    parser.add_argument('--lambda_l2', type=float, default=0.0005, help='Parameter to control L2 loss')
+    parser.add_argument('--lambda_l2', type=float, default=0.0, help='Parameter to control L2 loss')
 
     parser.add_argument('--train_act_func', type=str, default='softmax',
                         help='Output activation func Training: [softmax, sigmoid]')
@@ -492,7 +491,7 @@ if __name__ == "__main__":
     #logging
     parser.add_argument('--results_path', type=str, default='../results/', help='path to save metrics')
     parser.add_argument('--exp_name', type=str, default='dev',
-                        help='Addition to experiment name for logging, e.g. size of dataset [small, large]')
+                        help='Addition to experiment name for logging, e.g. size of dataset [dev, med, large]')
 
     config = parser.parse_args()
 
