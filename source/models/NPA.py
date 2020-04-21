088b157bbe108884e7e8ab679aaa877c5b98efb7
HB <bartsch.henning@gmail.com>
2020-04-21 14:01:36 +0200

    [Important]: refactor NPA and inherit from 'BaseModel'

--- source/models/NPA.py
+++ source/models/NPA.py
@@ -14,41 +14,12 @@ from source.modules.attention import PersonalisedAttentionWu
 from source.modules.interest_extractor import GRU as GRU_interest
 
 
-class BaseModelNPA(torch.nn.Module):
-
-    def __init__(self, news_encoder, interest_extractor, user_encoder, click_predictor, dim_uid_emb, dim_pref_query, word_embeddings, dim_word_embeddings, cnn_filter, device):
-        super(BaseModelNPA, self).__init__()
-
-        self.device = device
-
-        self.news_encoder = news_encoder
-        self.interest_extractor = interest_extractor
-        self.user_encoder = user_encoder
-        self.click_predictor = click_predictor
-
-
-        raise NotImplementedError()
-
-
-    def forward(self, browsing_history, user_id, candidate_news):
-
-        encoded_news = self.news_encoder.encode(browsing_history, user_id)
-
-        contextualised_news = self.interest_extractor(encoded_news)
-        user_representation = self.user_encoder.encode(contextualised_news, user_id)
-
-        encoded_candidates = self.news_encoder.encode(candidate_news, user_id)
-
-        score = self.click_predictor(user_representation, encoded_candidates)
-
-        return score
-
-class NPA_wu(nn.Module):
+class BaseModelNPA(nn.Module):
 
     def __init__(self, n_users, vocab_len, pretrained_emb, emb_dim_user_id=50, emb_dim_pref_query=200,
                  emb_dim_words=300, max_news_len=30, max_hist_len=50, n_filters_cnn=400, dropout_p=0.2, device='cpu',
                  news_encoder=None, user_encoder=None, interest_extractor=None, click_predictor=None):
-        super(NPA_wu, self).__init__()
+        super(BaseModelNPA, self).__init__()
 
         self.device = device
         self.vocab_len = vocab_len
@@ -128,6 +99,70 @@ class NPA_wu(nn.Module):
         if self.interest_extractor is not None:
             in_shape = encoded_brows_hist.shape
             encoded_brows_hist = torch.stack(self.interest_extractor(encoded_brows_hist), dim=2)
+
+        self.user_rep = self.user_encoder(encoded_brows_hist, pref_q_article)
+
+        return self.user_rep
+
+    def get_representation_shapes(self):
+        shapes = {}
+        shapes['user_rep'] = self.user_rep.shape if self.user_rep != None else None
+        shapes['brow_hist'] = self.brows_hist_reps.shape
+        shapes['cands'] = self.candidate_reps.shape
+        shapes['scores'] = self.click_scores.shape
+
+        for key, shape in shapes.items():
+            print("{} shape {}".format(key, shape))
+
+        return
+
+class VanillaNPA(BaseModelNPA):
+
+    def __init__(self, n_users, vocab_len, pretrained_emb, emb_dim_user_id=50, emb_dim_pref_query=200,
+                 emb_dim_words=300, max_news_len=30, max_hist_len=50, n_filters_cnn=400, dropout_p=0.2, device='cpu'):
+        super(VanillaNPA, self).__init__(n_users, vocab_len, pretrained_emb)
+
+        self.device = device
+        self.vocab_len = vocab_len
+        self.max_title_len = max_news_len
+        self.max_hist_len = max_hist_len
+
+        self.dim_news_rep = n_filters_cnn
+        self.dim_user_rep = n_filters_cnn
+        self.dim_emb_user_id = emb_dim_user_id
+        self.dim_pref_q = emb_dim_pref_query
+
+        #representations
+        self.user_rep = None
+        self.brows_hist_reps = None
+        self.candidate_reps = None
+        self.click_scores = None
+
+        if pretrained_emb is not None:
+            #assert pretrained_emb.shape == [vocab_len, emb_dim_words]
+            #print("Emb shape is {} and should {}".format(pretrained_emb.shape, (vocab_len, emb_dim_words)))
+            self.word_embeddings = nn.Embedding.from_pretrained(torch.FloatTensor(pretrained_emb), freeze=False, padding_idx=0)      # word embeddings
+        else:
+            self.word_embeddings = nn.Embedding(vocab_len, emb_dim_words)
+
+        self.user_id_embeddings = nn.Embedding(n_users, self.dim_emb_user_id, padding_idx=0)
+
+        self.news_encoder = NewsEncoderWuCNN(n_filters=n_filters_cnn, word_emb_dim=emb_dim_words, dim_pref_q=emb_dim_pref_query, dropout_p=dropout_p)
+
+        # preference queries
+        self.pref_q_word = PrefQueryWu(self.dim_pref_q, self.dim_emb_user_id)
+        self.pref_q_article = PrefQueryWu(self.dim_pref_q, self.dim_emb_user_id)
+
+        self.interest_extractor = None
+
+        self.user_encoder = PersonalisedAttentionWu(emb_dim_pref_query, self.dim_news_rep)
+
+        self.click_predictor = SimpleDot(self.dim_user_rep, self.dim_news_rep)
+
+
+    def create_user_rep(self, user_id, encoded_brows_hist):
+
+        pref_q_article = self.pref_q_article(self.user_id_embeddings(user_id))
 
         self.user_rep = self.user_encoder(encoded_brows_hist, pref_q_article)
 
