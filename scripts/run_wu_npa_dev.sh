468c8b7d6e7ebfd363025ce7ad67c91813d0ad5d
HB <bartsch.henning@gmail.com>
2020-04-21 15:33:47 +0200

    Add 'exp_name' to lisa script

--- scripts/run_wu_npa_dev.sh
+++ scripts/run_wu_npa_dev.sh
@@ -22,14 +22,15 @@ datapath="../datasets/dpg/dev_time_split_interactions/"
 embeddings="../embeddings/cc.nl.300.bin"
 train="wu"
 eval="wu"
+exp_name="dev_vanilla_npa"
 
-echo $datapath
+echo $exp_name
 
 for SEED in {42..45}
 do
   #1
   echo $SEED
-  python -u train_npa.py --data_path=$datapath --word_emb_path=$embeddings --exp_name="dev_vanilla_npa" \
+  python -u train_npa.py --data_path=$datapath --word_emb_path=$embeddings --exp_name=$exp_name \
     --npa_variant="vanilla" --random_seed=$SEED --n_epochs=10 --batch_size=100 --train_method=$train --eval_method=$eval
 
   python -u train_npa.py --data_path=$datapath --word_emb_path=$embeddings --exp_name="dev_l2_npa" \
