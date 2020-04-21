69b556ac267b90d78e6f42545f8152e9f114a6a8
HB <bartsch.henning@gmail.com>
2020-04-21 10:55:55 +0200

    [Important]: remove space to declare bash variable correctly, e.g. var_name="val"  (no space between equal sign!)

--- scripts/get_dpg_data_sample.sh
+++ scripts/get_dpg_data_sample.sh
@@ -19,8 +19,8 @@ python --version
 #srun -n 2 -t 00:30:00 --pty bash -il
 
 datapath="../datasets/dpg/"
-thresh_date = "24-12-2019-23-59-59"
+thresh_date="24-12-2019-23-59-59"
 
   #1
-python -u get_dpg_data_sample.py --data_dir=$datapath --save_dir=$datapath --size="medium" --time_threshold=$thresh_date
-python -u get_dpg_data_sample.py --data_dir=$datapath --save_dir=$datapath --size="dev" --time_threshold=$thresh_date
+python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="medium" --time_threshold=$thresh_date
+python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="dev" --time_threshold=$thresh_date
