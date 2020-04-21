03c3288bde8adc7df3121ab622e9f4ef9a33452e
HB <bartsch.henning@gmail.com>
2020-04-21 11:38:11 +0200

    Adjust train script to run Vanilla NPA

--- scripts/train_npa_dev.sh
+++ scripts/train_npa_dev.sh
@@ -25,22 +25,7 @@ datapath="../datasets/dpg/dev_time_split/"
 embeddings="../embeddings/cc.nl.300.bin"
 
 python -u train_npa.py --data_path=$datapath --word_emb_path=$embeddings --exp_name="dev" --train_method="wu"
-python -u train_npa.py --data_path=$datapath --word_emb_path=$embeddings --exp_name="dev_cut" --train_method="pos_cut_off" \
-  --test_act_func="softmax"
-python -u train_npa.py --data_path=$datapath --word_emb_path=$embeddings --exp_name="dev_cut" --train_method="pos_cut_off" \
-  --interest_extractor="gru" --test_act_func="softmax"
-
-#python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --exp_name="dev-softm" \
-#                  --eval_method="custom" --train_act_func="softmax" --test_act_func="softmax"
-#
-#python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" --exp_name="dev-sig" \
-#                  --eval_method="custom" --train_act_func="sigmoid" --test_act_func="sigmoid"
-#
-#python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" \
-#                  --weight_decay=0.0001 --exp_name="dev-wd" --eval_method="custom"
-#python -u train_npa.py --data_path="../datasets/dpg/dev_time_split/" --word_emb_path="../embeddings/cc.nl.300.bin" \
-#                  --weight_decay=0.001 --exp_name="dev-wd" --eval_method="custom"
-#python -u train_npa.py --data_path="../datasets/dpg/dev/" --word_emb_path="../embeddings/cc.nl.300.bin" --weight_decay=0.0001 --test_w_one=1 --random_seed=13 --exp_name="dev-wd"
-#python -u train_npa.py --data_path="../datasets/dpg/dev/" --word_emb_path="../embeddings/cc.nl.300.bin" --weight_decay=0.0001 --test_w_one=1 --random_seed=102 --exp_name="dev-wd"
-
-
+#python -u train_npa.py --data_path=$datapath --word_emb_path=$embeddings --exp_name="dev_cut" --train_method="pos_cut_off" \
+#  --test_act_func="softmax"
+#python -u train_npa.py --data_path=$datapath --word_emb_path=$embeddings --exp_name="dev_cut" --train_method="pos_cut_off" \
+#  --interest_extractor="gru" --test_act_func="softmax"
