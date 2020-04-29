ca4a768dd5f8791d9ccd251739455c63da27b1ac
HB <bartsch.henning@gmail.com>
2020-04-29 10:35:23 +0200

    Change data path from 'dpg' to 'DPG_dec19'

--- scripts/get_dpg_data_sample.sh
+++ scripts/get_dpg_data_sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=prep_dpg_data
 #SBATCH -n 4
-#SBATCH -t 04:20:00
+#SBATCH -t 01:20:00
 #SBATCH --mem=60000M
 
 module load pre2019
@@ -18,25 +18,25 @@ python --version
 
 #srun -n 2 -t 00:30:00 --pty bash -il
 
-datapath="../datasets/dpg/"
+datapath="../datasets/DPG_dec19/"
 thresh_date="24-12-2019-23-59-59"
 
   #1
-python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="dev" \
---time_threshold=$thresh_date --item_sample_method="random"
-
-python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="dev" \
---time_threshold=$thresh_date --item_sample_method="most_common" --overwrite_existing=0
-  #3
 python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="medium" \
---time_threshold=$thresh_date --item_sample_method="random" --overwrite_existing=0
+--time_threshold=$thresh_date --item_sample_method="wu"
 
-python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="medium" \
---time_threshold=$thresh_date --item_sample_method="most_common" --overwrite_existing=0
-
-  #5
-python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="custom" \
---time_threshold=$thresh_date --n_users=10000 --n_articles=100000 --item_sample_method="most_common"
+#python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="dev" \
+#--time_threshold=$thresh_date --item_sample_method="most_common" --overwrite_existing=0
+#  #3
+#python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="medium" \
+#--time_threshold=$thresh_date --item_sample_method="random" --overwrite_existing=0
+#
+#python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="medium" \
+#--time_threshold=$thresh_date --item_sample_method="most_common" --overwrite_existing=0
+#
+#  #5
+#python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="custom" \
+#--time_threshold=$thresh_date --n_users=10000 --n_articles=100000 --item_sample_method="most_common"
 
 #python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="medium" --time_threshold=$thresh_date
 #python -u get_dpg_data_sample.py --data_dir=$datapath --save_path=$datapath --size="dev" --time_threshold=$thresh_date
